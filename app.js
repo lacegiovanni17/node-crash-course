62f89ea6c143e45a9a6566704af50fd4f229e779
Chidike Henry <dk1online25@gmail.com>
2023-02-15 10:16:02 +0100

    added new feat to app

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ const app = express();
 //register view engine
 app.set('view engine', ejs);
 
-//listen for requests
-app.listen(3000);
+
 
 app.get('/', (req, res) => {
     res.render('index');
@@ -16,3 +15,18 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about');
 })
+
+//redirects
+app.get('/about-us', (req, res) => {
+    res.redirect('/about');
+})
+
+//404 page
+app.use((req, res) => {
+    res.status(404).render('404');
+});
+
+
+
+//listen for requests
+app.listen(3000);
